34f85ef529cf42194e51de042a1da3feb3c68c3e
Hackbright Student <no-reply@hackbrightacademy.com>
2017-06-21 12:59:11 -0700

    Add functionality for adding users

--- server.py
+++ server.py
@@ -34,6 +34,47 @@ def user_list():
     users = User.query.all()
     return render_template("user_list.html", users=users)
 
+@app.route("/register", methods=["GET"])
+def register_form():
+
+
+    return render_template("register_form.html")
+
+@app.route("/register", methods=["POST"])
+def register_process():
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+
+    if User.query.filter_by(email=email).all():
+        #work on notifying user
+        flash("User e-mail already exists. Please choose a new e-mail.")
+    else:
+        sql = """INSERT INTO users (email, password)
+                 VALUES (:email, :password)
+              """
+
+        db.session.execute(sql,
+                            {'email': email,
+                            'password': password})
+
+        db.session.commit()                       
+
+    return redirect("/")
+
+@app.route("/login", methods=["GET"])
+def login_form():
+
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login_process():
+
+    email = request.form.get("email")
+
+
+
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
